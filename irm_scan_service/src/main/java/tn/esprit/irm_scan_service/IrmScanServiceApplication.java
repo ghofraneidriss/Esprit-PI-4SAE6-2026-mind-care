4623793bc697ea58d02fd7c69f174c76a8146006
ghofrane <Ghofrane.Idriss@esprit.tn>
2026-02-11 21:17:54 +0100

    microservice irm_rapport

---
+++ irm_scan_service/src/main/java/tn/esprit/irm_scan_service/IrmScanServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.irm_scan_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrmScanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrmScanServiceApplication.class, args);
+    }
+
+}
---
+++ irm_scan_service/src/test/java/tn/esprit/irm_scan_service/IrmScanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.irm_scan_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrmScanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
