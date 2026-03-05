acc5c56ba31805a696b4320ed1c371cf650c7c55
ghofrane <Ghofrane.Idriss@esprit.tn>
2026-03-05 11:04:03 +0100

    merge

--- front/src/app/frontoffice/incidents/incidents.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-frontoffice-incidents',
-  standalone: false,
-  templateUrl: './incidents.html',
-  styleUrls: ['./incidents.css'],
-})
-export class FrontofficeIncidentsPage {}
