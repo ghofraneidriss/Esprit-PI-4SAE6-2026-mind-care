96fa94f89fcb7a0d1e7c16fd3f8d571c086ad074
ghofrane <Ghofrane.Idriss@esprit.tn>
2026-04-27 18:34:50 +0100

    jesnkis fixes

--- medical_report_service/src/test/java/tn/esprit/medical_report_service/MedicalReportServiceApplicationTests.java
+++ medical_report_service/src/test/java/tn/esprit/medical_report_service/MedicalReportServiceApplicationTests.java
@@ -2,8 +2,10 @@ package tn.esprit.medical_report_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MedicalReportServiceApplicationTests {
 
     @Test
--- volunteer/src/test/java/tn/esprit/microservice/volunteer/VolunteerApplicationTests.java
+++ volunteer/src/test/java/tn/esprit/microservice/volunteer/VolunteerApplicationTests.java
@@ -2,8 +2,10 @@ package tn.esprit.microservice.volunteer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VolunteerApplicationTests {
 
     @Test
