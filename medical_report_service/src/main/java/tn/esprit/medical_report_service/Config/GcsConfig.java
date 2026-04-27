3389fdf634b2c153857cfdb111ec7852744b66b4
ghofrane <Ghofrane.Idriss@esprit.tn>
2026-04-27 19:09:06 +0100

    jesnkis fixes

--- medical_report_service/src/main/java/tn/esprit/medical_report_service/Config/GcsConfig.java
+++ medical_report_service/src/main/java/tn/esprit/medical_report_service/Config/GcsConfig.java
@@ -1,5 +1,6 @@
 package tn.esprit.medical_report_service.Config;
 
+import com.google.cloud.NoCredentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class GcsConfig {
@@ -14,17 +16,23 @@ public class GcsConfig {
     @Value("${gcs.project-id}")
     private String projectId;
 
-    @Value("${gcs.credentials.path}")
+    @Value("${gcs.credentials.path:}")
     private Resource credentialsResource;
 
     @Bean
     public Storage storage() throws Exception {
-        GoogleCredentials credentials =
-                GoogleCredentials.fromStream(credentialsResource.getInputStream());
+        StorageOptions.Builder storageOptions = StorageOptions.newBuilder()
+                .setProjectId(projectId);
 
-        return StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(credentials)
+        if (credentialsResource != null && StringUtils.hasText(credentialsResource.getFilename())) {
+            GoogleCredentials credentials =
+                    GoogleCredentials.fromStream(credentialsResource.getInputStream());
+            storageOptions.setCredentials(credentials);
+        } else {
+            storageOptions.setCredentials(NoCredentials.getInstance());
+        }
+
+        return storageOptions
                 .build()
                 .getService();
     }
